46ab51be900970bb2addee47e6fa55714640bf7a
adamant <adamant.pwn@gmail.com>
2015-10-03 11:09:23 +0300

    Fixed.

--- s3_exec.cpp
+++ s3_exec.cpp
@@ -1,52 +1,50 @@
 #include <iostream>
 #include <string.h>
+#include <stdlib.h>
 #include <sstream>
 #include <vector>
 #include <unistd.h>
 #include <wait.h>
-#include <stdlib.h>
 
 using std::string;
 using std::stringstream;
 using std::vector;
 using std::cin;
 using std::cout;
-using std::allocator;
 
-/*
- * надо завести структуру Task, где будут поля time, name, argv.
- * просто без контекста неясно, что за name, time ... вы хотите считать
- */
-
-void scan_task(int &time, string &name, vector<char*> &argv)
+struct Task
 {
-	cin >> time;
-	cin >> name;
-/*
- * что именно вы вкладываете в перенос 1 в первый аргумент сложения. выглядит непривычно.
- */
-	argv.push_back((char*)malloc(1 + name.size()));
-	memcpy(argv.back(), name.c_str(), name.size());
-	
+	int time;
+	string name;
+	vector<string> argv;
+};
+
+using namespace std;
+
+void scan_task(Task &task)
+{
+	cin >> task.time;
+	cin >> task.name;
+	task.argv.push_back(task.name);
 	string tmp;
 	getline(cin, tmp);
 	stringstream parser(tmp);
 	while(parser >> tmp)
-	{
-		argv.push_back((char*)malloc(1 + tmp.size()));
-		memcpy(argv.back(), tmp.c_str(), tmp.size());
-	}
-	argv.push_back(NULL);
+		task.argv.push_back(tmp);
 }
 
-int start_task(int time, string &name, vector<char*> &argv)
-{	
+int start_task(Task &task)
+{
+	vector<char*> argv;
+	for(auto &it: task.argv)
+		argv.push_back((char*)it.data());
+	argv.push_back(NULL);
 	pid_t pid = fork();
 	if(pid == 0)
 	{
-		sleep(time);
+		sleep(task.time);
 		cout << "Process " << getpid() << " started.\n";
-		execvp(name.c_str(), argv.data());
+		execvp(task.name.data(), argv.data());
 	}
 	return pid;
 }
@@ -55,31 +53,14 @@ int main()
 {
 	int task_count;
 	cin >> task_count;
-  
-  /*
-   * Глаголами нужно называть только ф-и. Переменные - существительные.
-   * Кого ждать?
-   */
-	vector<int> wait;
+	vector<int> task_pids;
 	for(int i = 0; i < task_count; i++)
 	{
-		int time;
-		string name;
-		vector<char*> argv;
-		scan_task(time, name, argv);
-/*
- * Я имел ввиду, что вы создаете как и раньше std::vector< std::string > parameters; и считываете в него.
- * Перед тем, как вызвать exec, создаете std::vector< char* > argv;
- * ...
- * argv[i] = parameters[i].data();
- * ...
- * т.е. просто конвертируете в то, что можно будет передать в exec, а в предыдущих местах работаете с std::string
- * Просто вы начали вручную выделять память malloc'ом, и ,конечно же, забыли её почистить.
- * А так бы она автоматически почистилась при вызове деструкторов.
- */
-    wait.push_back(start_task(time, name, argv));
+		Task task;
+		scan_task(task);
+		task_pids.push_back(start_task(task));
 	}
-	for(auto task: wait)
+	for(auto task: task_pids)
 	{
 		int status;
 		waitpid(task, &status, 0);
