b52162005e339ae00dd81b5d317f1485eea1a5a8
adamant <adamant.pwn@gmail.com>
2015-12-10 12:20:27 +0300

    file searcher

---
+++ s9_search.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <iostream>
+#include <sys/stat.h>
+
+using std::cout;
+using std::string;
+
+void search(string path, int depth, string needle)
+{
+	if(depth < 0)
+		return;
+	
+ 
+    DIR *dir = opendir(path.c_str());
+    dirent *file;
+    while(file = readdir(dir))
+    {
+		if(string(file->d_name) == "." || string(file->d_name) == "..")
+			continue;
+		if(file->d_name == needle)
+		{
+			cout << "File found at \"" << path << "\"!\n";
+			exit(0);
+		}
+		struct stat info;
+		stat(file->d_name, &info);
+		if(S_ISDIR(info.st_mode))
+			search(path + "/" + file->d_name, depth - 1, needle);
+	}
+    closedir(dir);
+}
+ 
+int main(int argc, char *argv[])
+{
+	string start_dir = argv[1];
+	int depth = atoi(argv[2]);
+	string needle = argv[3];
+	search(start_dir, depth, needle);
+	cout << "File not found!\n";
+    return 0;
+}
