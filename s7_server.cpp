98513c0dea503db0f949b201ed6c0c82bc540b28
adamant <adamant.pwn@gmail.com>
2015-12-15 01:59:59 +0300

    fixed

--- s7_server.cpp
+++ s7_server.cpp
@@ -16,25 +16,11 @@ using std::vector;
 
 const int maxn = 10;
 
-/*
- * В целом, код рабочий, но многое не нравится:
- * 1) У вас locked_vector - это вектор интов, что никак не отражено в названии. Если вы пишите на С++, что 
- * вам мешало сделать locked_vector также шаблонным.
- * 2) while(this->empty()); ? бросайте исключения, падайте на assert`e ... Зависнуть в бесконечном цикле - вариант только на временя отладки. Отладили - удалите.
- * 3) available - это прилагательное. в названии переменных и структур должно быть существительное.
- * 4) mymsgbuf - это было написано в примере из учебника. на учебник не надо ровняться с точки зрения стиля написания кода. Из названия этой структуры совершенно неясно, зачем она нужна.
- * 5) void *thread(void *arg) - в названии ф-й и методов должен быть глагол.
- * 6) по возможности надо разбивать код на небольшие ф-и с понятными названиями, что делает код самодокументируемым.
- * 
- * Если бы зачёты не надо было проставить до 19 числа, то код надо было бы явно править.
- * Пока пусть так.
- */
-
-class locked_vector : vector<int>
+template<class T>class locked_vector : public vector<T>
 {
 	std::mutex vector_mutex;
 	public:
-	void safety_push_back(int elem)
+	void safety_push_back(T elem)
 	{
 		vector_mutex.lock();
 		this->push_back(elem);
@@ -43,7 +29,6 @@ class locked_vector : vector<int>
 	
 	int safety_pop()
 	{
-		while(this->empty());
 		vector_mutex.lock();
 		int ret = this->back();
 		this->pop_back();
@@ -52,49 +37,50 @@ class locked_vector : vector<int>
 	}
 };
 
-locked_vector available;
+locked_vector<int> available_ids;
 
 key_t key = ftok("shared_memory", 0); 
 int msgid = msgget(key, 0666 | IPC_CREAT);
 
-struct mymsgbuf
+struct msg_t
 {
 	long type;
 	int num[2];
 } query[maxn];
 
-void *thread(void *arg)
+void *answer_query(void *arg)
 {
-	mymsgbuf mybuf = *(mymsgbuf*)arg;
-	mybuf.type += maxn;
-	mybuf.num[0] *= mybuf.num[1];
-	msgsnd(msgid, &mybuf, sizeof(mybuf.num), 0);
-	available.safety_push_back(mybuf.type - maxn);
+	msg_t msg = *(msg_t*)arg;
+	msg.type += maxn;
+	msg.num[0] *= msg.num[1];
+	msgsnd(msgid, &msg, sizeof(msg.num), 0);
+	available_ids.safety_push_back(msg.type - maxn);
 }
 	
 int main()
 {
 	cout << "Hello! I'm the server and I'm ready to process your queries!\n";
 
-	mymsgbuf mybuf;
+	msg_t msg;
 	
 	for(int i = 3; i < maxn; i++)
-		available.safety_push_back(i);
+		available_ids.safety_push_back(i);
 	
 	while(1)
 	{
-		msgrcv(msgid, &mybuf, sizeof(mybuf.num), -maxn, 0);
-		if(mybuf.type == 1)
+		msgrcv(msgid, &msg, sizeof(msg.num), -maxn, 0);
+		if(msg.type == 1) // client is asking for id
 		{
-			mybuf.type = maxn + 1;
-			mybuf.num[0] = available.safety_pop();
-			msgsnd(msgid, &mybuf, sizeof(mybuf.num), 0);
+			msg.type = maxn + 1;
+			while(available_ids.empty()); // wait for some thread to free the ID
+			msg.num[0] = available_ids.safety_pop();
+			msgsnd(msgid, &msg, sizeof(msg.num), 0);
 		}
-		else
+		else // client is asking for query answer
 		{
-			query[mybuf.type] = mybuf;
+			query[msg.type] = msg;
 			pthread_t id;
-			pthread_create(&id, NULL, thread, &query[mybuf.type]);
+			pthread_create(&id, NULL, answer_query, &query[msg.type]);
 		}
 	}
 	
